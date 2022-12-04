9ef4100204fc62ee036310a681fea4a13595b6f1
MaxDeykalo <108637289+MaxDeykalo@users.noreply.github.com>
2022-12-03 22:13:48 -0500

    earth

--- ContentView.swift
+++ ContentView.swift
@@ -10,8 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-
-            Text("Hello, world!")
+            Text("Hello, earth!")
         }
         .padding()
     }
